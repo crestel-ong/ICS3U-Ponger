f0963e8168add79190da8e53cd2c6ed23e13ba47
Crestel Ong <90276376+crestel-ong@users.noreply.github.com>
2021-10-24 21:44:51 -0400

    lesson 2 (pong1)

--- main.lua
+++ main.lua
@@ -1,14 +1,40 @@
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+push = require 'push'
+
+--[[
+    Runs when the game first starts up, only once; used to initilize the game.
+]]
 function love.load()
-    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         vsync = true,
         resizable = false
-    } )
+    })
 end
 
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
+--[[
+    Called after update by LÖVE, used to draw anything to the screen, updated or ortherwise.
+]]
 function love.draw()
-    love.graphics.printf("Hello, Pong!", 0, WINDOW_HEIGHT / 2 - 6, WINDOW_WIDTH, 'center')
+    push:apply('start')
+
+    love.graphics.printf(
+      "Hello, Pong!",           -- text to render
+       0,                       -- starting X (0 since we're going to center it based on width)
+       VIRTUAL_HEIGHT / 2 - 6,   -- starting Y (halfway down the screen)
+       VIRTUAL_WIDTH,            -- number of pixels to center within (the entire screen here)
+       'center')                -- alignment mode, can be 'center', 'left', or 'right'
+
+    push:apply('end')
 end
